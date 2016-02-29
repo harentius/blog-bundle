3e8f947a390094dbb02b14f61e1e48c27addfa18
Yaroslav Osypenko <harentius@yandex.ru>
2016-02-29 22:48:15 +0200

    corrections

---
+++ ViewsCounter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Harentius\BlogBundle;
+
+use Doctrine\ORM\EntityManager;
+use Harentius\BlogBundle\Entity\Article;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class ViewsCounter
+{
+    /**
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    /**
+     * @param RequestStack $requestStack
+     * @param EntityManager $em
+     */
+    public function __construct(RequestStack $requestStack, EntityManager $em)
+    {
+        $this->request = $requestStack->getCurrentRequest();
+        $this->em = $em;
+    }
+
+    /**
+     * @param Article $article
+     */
+    public function processArticle(Article $article)
+    {
+        $session = $this->request->getSession();
+        $viewedArticles = $session->get('viewedArticles', []);
+        $articleId = $article->getId();
+
+        if (!isset($viewedArticles[$articleId] )) {
+            $viewedArticles[$articleId] = true;
+            $article->increaseViewsCount();
+            $session->set('viewedArticles', $viewedArticles);
+        }
+    }
+}
