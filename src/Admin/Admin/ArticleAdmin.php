4fe9679e8ad94da69b71b1e07f751583cf3f28e3
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2019-12-05 22:07:45 +0200

    update stylings, templates

--- src/Admin/Admin/ArticleAdmin.php
+++ src/Admin/Admin/ArticleAdmin.php
@@ -126,8 +126,5 @@ class ArticleAdmin extends AbstractPostAdmin
     private function clearCache()
     {
         $this->controllersCache->deleteAll();
-        $container = $this->getConfigurationPool()->getContainer();
-//        $container->get('harentius_blog.router.category_slug_provider')->clearAll();
-//        $container->get('harentius_blog.controller.feed_cache')->deleteAll();
     }
 }
