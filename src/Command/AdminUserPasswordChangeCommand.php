e3d5af7dce58cd4855a41a06d7a388d39035d8a9
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2020-01-04 22:34:16 +0200

    refactor command

--- src/Command/AdminUserPasswordChangeCommand.php
+++ src/Command/AdminUserPasswordChangeCommand.php
@@ -2,21 +2,44 @@
 
 namespace Harentius\BlogBundle\Command;
 
-use Harentius\BlogBundle\Entity\AdminUser;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Doctrine\ORM\EntityManagerInterface;
+use Harentius\BlogBundle\Entity\AdminUserRepository;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class AdminUserPasswordChangeCommand extends ContainerAwareCommand
+class AdminUserPasswordChangeCommand extends Command
 {
+    protected static $defaultName = 'blog:admin-user-password:change';
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * @var AdminUserRepository
+     */
+    private $adminUserRepository;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @param AdminUserRepository $adminUserRepository
+     */
+    public function __construct(EntityManagerInterface $entityManager, AdminUserRepository $adminUserRepository)
+    {
+        $this->entityManager = $entityManager;
+        $this->adminUserRepository = $adminUserRepository;
+        parent::__construct();
+    }
+
     /**
      * {@inheritdoc}
      */
     protected function configure()
     {
         $this
-            ->setName('blog:admin-user-password:change')
             ->addArgument('password', InputArgument::REQUIRED)
         ;
     }
@@ -26,16 +49,14 @@ class AdminUserPasswordChangeCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
-        /** @var AdminUser $adminUser */
-        $adminUser = $em->getRepository('HarentiusBlogBundle:AdminUser')->findOneBy(['username' => 'admin']);
+        $adminUser = $this->adminUserRepository->findOneBy(['username' => 'admin']);
 
-        if ($adminUser) {
+        if (!$adminUser) {
             throw new \RuntimeException('Admin User not found');
         }
 
         $adminUser->setPlainPassword($input->getArgument('password'));
-        $em->flush();
+        $this->entityManager->flush();
 
         $output->writeln('Password successfully changed');
     }
