124b4369c100e17ca11e5161fec50e13f16029a7
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2020-01-14 22:05:31 +0200

    cs corrections

---
+++ tests/Unit/Controller/ImageOptimizationControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Harentius\BlogBundle\Tests\Unit\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+class ImageOptimizationControllerTest extends TestCase
+{
+    private function create()
+    {
+    }
+}
