c25ccb1207629f1361d5f73b62d6aa05ce887823
Yaroslav Osypenko <yaroalv.osypenko@gmail.com>
2024-09-15 19:21:10 +0200

    make cover image optional

--- src/Twig/CoverImageExtension.php
+++ src/Twig/CoverImageExtension.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace Harentius\BlogBundle\Twig;
 
+use Symfony\Component\Filesystem\Filesystem;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class CoverImageExtension extends AbstractExtension
 {
-    public function __construct()
-    {
+    public function __construct(
+        private readonly string $targetDir,
+    ) {
     }
 
     /**
@@ -20,11 +22,20 @@ class CoverImageExtension extends AbstractExtension
     {
         return [
             new TwigFunction('cover_image_path', $this->getImagePath(...)),
+            new TwigFunction('cover_image_exists', $this->coverImageExists(...)),
         ];
     }
 
     public function getImagePath(string $articleSlug): ?string
     {
         return '/assets/' . $articleSlug . '/cover.png';
+    }
+
+    public function coverImageExists(string $articleSlug): bool
+    {
+        $filesystem = new Filesystem();
+        $coverFilePath = $this->targetDir . '/' . $articleSlug . '/cover.png';
+
+        return $filesystem->exists($coverFilePath);
     }
 }
